ab5edd504789e8fad3dcf0f30b0fbec8608e2abe
Benjamin Bach <benjaoming@gmail.com>
2015-04-27 01:04:38 +0200

    Use list instead of patterns()

--- django_nyt/urls.py
+++ django_nyt/urls.py
@@ -2,19 +2,23 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django import VERSION as DJANGO_VERSION
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    '', url('^json/get/$', 'django_nyt.views.get_notifications',
-            name='json_get'), url('^json/get/(?P<latest_id>\d+)/$',
-                                  'django_nyt.views.get_notifications', name='json_get'), url(
-        '^json/mark-read/$', 'django_nyt.views.mark_read',
-        name='json_mark_read_base'), url('^json/mark-read/(\d+)/$',
-                                         'django_nyt.views.mark_read', name='json_mark_read'), url(
-        '^json/mark-read/(?P<id_lte>\d+)/(?P<id_gte>\d+)/$',
-        'django_nyt.views.mark_read', name='json_mark_read'), url(
-        '^goto/(?P<notification_id>\d+)/$', 'django_nyt.views.goto', name='goto'),
-    url('^goto/$', 'django_nyt.views.goto', name='goto_base'),)
+urlpatterns = [
+    url('^json/get/$', 'django_nyt.views.get_notifications', name='json_get'),
+    url('^json/get/(?P<latest_id>\d+)/$', 'django_nyt.views.get_notifications', name='json_get'),
+    url('^json/mark-read/$', 'django_nyt.views.mark_read', name='json_mark_read_base'),
+    url('^json/mark-read/(\d+)/$', 'django_nyt.views.mark_read', name='json_mark_read'),
+    url('^json/mark-read/(?P<id_lte>\d+)/(?P<id_gte>\d+)/$', 'django_nyt.views.mark_read', name='json_mark_read'),
+    url('^goto/(?P<notification_id>\d+)/$', 'django_nyt.views.goto', name='goto'),
+    url('^goto/$', 'django_nyt.views.goto', name='goto_base'),
+]
+
+
+if DJANGO_VERSION < (1, 8):
+    from django.conf.urls import patterns
+    urlpatterns = patterns('', *urlpatterns)
 
 
 def get_pattern(app_name="nyt", namespace="nyt"):
