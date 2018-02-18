5a59a2a30acd61821c57982c9399e269fd62fce6
Raffaele Salmaso <raffaele@salmaso.org>
2018-02-18 17:43:42 +0100

    Update admin urls inclusion

--- test-project/testproject/urls.py
+++ test-project/testproject/urls.py
@@ -13,7 +13,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'', include('testapp.urls')),
 ]
 
