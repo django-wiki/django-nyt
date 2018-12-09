1afa3c35f10c29870363593e07c4e1aa4b54a010
yevheniidehtiar <yevhenii.dehtiar@gmail.com>
2018-12-10 01:57:56 +0200

    create one router for django integration

--- django_nyt/routing.py
+++ django_nyt/routing.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
-from channels.routing import URLRouter
+from django.urls import path
 from channels.auth import AuthMiddlewareStack
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from . import consumers
 
-channel_routing = AuthMiddlewareStack(
-    URLRouter([
-        url(r"^nyt/$", consumers.NytConsumer),
-    ])
-)
+
+main_router = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('nyt', consumers.NytConsumer),
+        ])
+    )
+})
