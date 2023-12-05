22af4b42d59428a5a020a11dacc68f28ae960b3b
Benjamin Bach <benjamin@overtag.dk>
2023-12-05 12:29:34 +0100

    Adds a test with a URL for the notification

--- tests/core/test_email.py
+++ tests/core/test_email.py
@@ -2,7 +2,6 @@ from django.core import mail
 from django.core.management import call_command
 
 from .test_basic import NotifyTestBase
-from django_nyt import models
 from django_nyt import utils
 
 
@@ -35,5 +34,31 @@ example.com
 """
         )
 
-        # Check that there is exactly 1 notification
-        self.assertEqual(models.Notification.objects.all().count(), 1)
+    def test_notify_with_url(self):
+        mail.outbox = []
+
+        # Subscribe User 1 to test key
+        utils.subscribe(self.user1_settings, self.TEST_KEY, send_emails=True)
+        utils.notify("Test is a test", self.TEST_KEY, url="/test")
+
+        call_command("notifymail", "--cron", "--no-sys-exit")
+
+        assert len(mail.outbox) == 1
+        assert mail.outbox[0].subject == "You have new notifications"
+        assert (
+            mail.outbox[0].body
+            == """Dear alice,
+
+These are the instantly notifications from example.com.
+
+ * Test is a test
+   https://example.com/test
+
+
+Thanks for using our site!
+
+Sincerely,
+example.com
+
+"""
+        )
