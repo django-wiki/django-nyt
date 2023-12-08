950004111190dba87f9cf6b3bbe5f250e886ef18
Benjamin Bach <benjamin@overtag.dk>
2023-12-08 20:39:30 +0100

    Add some comments

--- django_nyt/settings/__init__.py
+++ django_nyt/settings/__init__.py
@@ -4,7 +4,13 @@ from . import defaults
 
 
 def __getattr__(name):
+    """
+    The attribute getter for this module makes it possible to
+    lazily/dynamically access the settings defined in django_nyt.settings.defaults.
+    """
 
+    # All attributes accessed with this prefix are possible to overwrite
+    # through django.conf.settings.
     settings_prefix = "NYT_"
 
     if name in defaults.__dict__.keys():
